13a26f473b8efbccbf662619467765eded20b89a
Nikolay.Burnykov <nikolay.burnykov@orange.com>
2022-03-04 13:02:37 +0300

    refactoring

--- dynamic/opt_bst/opt_bst.py
+++ dynamic/opt_bst/opt_bst.py
@@ -3,7 +3,7 @@ from typing import List, Dict
 
 class Symmetric2DArray:
     """
-    A structure that emulates square 2D array which is symmetric but makes this in optimal way
+    A structure that emulates square 2D array which is symmetric and makes this in optimal way in terms of space
     """
 
     def __init__(self, width_height: int):
@@ -13,22 +13,21 @@ class Symmetric2DArray:
             self.offsets[width_height - i] = lng
             lng += i
 
-        self.data = [None] * lng
+        self.data = [None] * lng  # a single array to store the data
         self.width_height = width_height
 
     def set_element(self, i: int, j: int, value: float):
-        if j < i:
-            i, j = j, i
-        j = j - i
-
-        self.data[self.offsets[i] + j] = value
+        self.data[self._get_offset(i, j)] = value
 
     def get_element(self, i: int, j: int) -> float:
+        return self.data[self._get_offset(i, j)]
+
+    def _get_offset(self, i: int, j: int) -> int:
         if j < i:
             i, j = j, i
         j = j - i
-
-        return self.data[self.offsets[i] + j]
+        o = self.offsets[i] + j
+        return o
 
 
 def opt_bst(data: List[tuple]) -> object:
